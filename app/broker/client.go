5e91005e37d2663d55af55675725211364d9c048
KumKeeHyun <kbzjung359@gmail.com>
2020-10-20 21:42:02 +0900

    getoffset testing

--- app/broker/client.go
+++ app/broker/client.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 	"log"
+	"sync"
 	"time"
 
 	"github.com/KumKeeHyun/kafka-head/app/helper"
@@ -15,7 +16,6 @@ type Partition struct {
 	PartitionID int32
 	Leader      int32
 	Replicas    []int32
-	Offset      int64
 }
 
 type TopicPartitions map[string][]Partition
@@ -29,6 +29,26 @@ func (tp TopicPartitions) String() string {
 		}
 	}
 	return res
+}
+
+func (tp *TopicPartitions) getOffsetRequest(client sarama.Client) (map[int32]*sarama.OffsetRequest, map[int32]*sarama.Broker) {
+	requests := make(map[int32]*sarama.OffsetRequest)
+	brokers := make(map[int32]*sarama.Broker)
+
+	for topic, partitions := range *tp {
+		for _, partition := range partitions {
+			broker, err := client.Leader(topic, partition.PartitionID)
+			if err != nil {
+				continue
+			}
+			if _, ok := requests[broker.ID()]; !ok {
+				requests[broker.ID()] = &sarama.OffsetRequest{}
+			}
+			brokers[broker.ID()] = broker
+			requests[broker.ID()].AddBlock(topic, partition.PartitionID, sarama.OffsetNewest, 1)
+		}
+	}
+	return requests, brokers
 }
 
 type SaramaBroker struct {
@@ -65,12 +85,21 @@ func (kafka *SaramaBroker) Start() error {
 	}
 	kafka.saramaClient = client
 
-	go kafka.startFetchBrokerInfo()
+	go kafka.startFetchBroker()
+	// kafka.startFetchBrokerTest()
 
 	return nil
 }
 
-func (kafka *SaramaBroker) startFetchBrokerInfo() {
+func (kafka *SaramaBroker) startFetchBrokerTest() {
+	kafka.fetchBrokerInfo()
+	// kafka.fetchOffset()
+	kafka.requestOffset()
+
+	kafka.Stop()
+}
+
+func (kafka *SaramaBroker) startFetchBroker() {
 	for {
 		select {
 		case <-kafka.fetchTicker.C:
@@ -116,4 +145,56 @@ func (kafka *SaramaBroker) fetchBrokerInfo() {
 
 	kafka.topicPartitions = topicPartitions
 	fmt.Println(kafka.topicPartitions)
+}
+
+func (kafka *SaramaBroker) fetchOffset() {
+	wg := sync.WaitGroup{}
+
+	fetchOffsetTopic := func(t string, ps []Partition) {
+		defer wg.Done()
+
+		for _, p := range ps {
+			offset, err := kafka.saramaClient.GetOffset(t, p.PartitionID, sarama.OffsetNewest)
+			if err != nil {
+				log.Printf("failed to getoffset : %s(%d)\n", t, p.PartitionID)
+				continue
+			}
+			log.Printf("topic : %s, partition : %d, offset : %d\n", t, p.PartitionID, offset)
+		}
+	}
+
+	for topic, partitions := range kafka.topicPartitions {
+		wg.Add(1)
+		go fetchOffsetTopic(topic, partitions)
+	}
+	wg.Wait()
+}
+
+func (kafka *SaramaBroker) requestOffset() {
+	requests, brokers := kafka.topicPartitions.getOffsetRequest(kafka.saramaClient)
+	wg := sync.WaitGroup{}
+
+	getBrokerOffsets := func(brokerID int32, request *sarama.OffsetRequest) {
+		defer wg.Done()
+		response, err := brokers[brokerID].GetAvailableOffsets(request)
+		if err != nil {
+			brokers[brokerID].Close()
+			return
+		}
+		for topic, partitions := range response.Blocks {
+			for partition, offsetResponse := range partitions {
+				if offsetResponse.Err != sarama.ErrNoError {
+					continue
+				}
+				log.Printf("topic : %s, partition : %d, offset : %d\n", topic, partition, offsetResponse.Offsets[0])
+			}
+		}
+	}
+
+	for brokerID, request := range requests {
+		wg.Add(1)
+		go getBrokerOffsets(brokerID, request)
+	}
+
+	wg.Wait()
 }
--- app/broker/client_test.go
+++ app/broker/client_test.go
@@ -19,3 +19,13 @@ func TestFetchBrokerInfo(t *testing.T) {
 	time.Sleep(13 * time.Second)
 	brokerClient.Stop()
 }
+
+// func TestFetchOffset(t *testing.T) {
+// 	brokerClient := broker.SaramaBroker{}
+// 	brokerClient.Setup()
+
+// 	err := brokerClient.Start()
+// 	if err != nil {
+// 		t.Fatal(err)
+// 	}
+// }
